58e23abb9c5f8972a3227c191e5471feb1f0c80c
pranjalshinde1401 <120730321+pranjalshinde1401@users.noreply.github.com>
2025-04-29 22:42:08 +0530

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,50 +1,11 @@
 <?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader (created by composer, not included with PHPMailer)
-require 'C:\xampp\htdocs\EmployeeForm\PHPMailer\src\PHPMailer.php';
-require 'C:\xampp\htdocs\EmployeeForm\PHPMailer\src\SMTP.php';
-require 'C:\xampp\htdocs\EmployeeForm\PHPMailer\src\Exception.php';
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'pranjalshinde1401@gmail.com';                     //SMTP username
-    $mail->Password   = 'fvdh bjro qpco idkh';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('pranjalshinde1401@gmail.com', 'pranjal');
-    $mail->addAddress('sidshinde2901@gmail.com', 'sid');     //Add a recipient
-   
-  
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'employee data form';
-    $mail->Body    = '';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect form data
+if(isset($_POST['submit']))
+{
     $emp_code = $_POST['emp_code'];
     $emp_name = $_POST['emp_name'];
     $department = $_POST['department'];
@@ -55,43 +16,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sap_id5 = $_POST['sap_id5'];
     $usage = $_POST['usage'];
     $location = $_POST['location'];
+  
+    $os_id = php_uname(); // Returns the operating system name (e.g., Linux, Windows NT)
 
-    // Detect operating system from user agent
-    $userAgent = $_SERVER['HTTP_USER_AGENT'];
-    $os = "Unknown OS";
+    
 
-    if (preg_match('/linux/i', $userAgent)) $os = "Linux";
-    elseif (preg_match('/macintosh|mac os x/i', $userAgent)) $os = "Mac";
-    elseif (preg_match('/windows|win32/i', $userAgent)) $os = "Windows";
+//Load Composer's autoloader (created by composer, not included with PHPMailer)
+require 'C:\xampp\htdocs\EmployeeForm\PHPMailer\PHPMailer.php';
+require 'C:\xampp\htdocs\EmployeeForm\PHPMailer\SMTP.php';
+require 'C:\xampp\htdocs\EmployeeForm\PHPMailer\Exception.php';
 
-    // Email content
-    $body = "
-Employee Code: $emp_code
-Employee Name: $emp_name
-Department: $department
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-SAP Login IDs:
-1. $sap_id1
-2. $sap_id2
-3. $sap_id3
-4. $sap_id4
-5. $sap_id5
+try {
+    //Server settings
+                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'pranjalshinde1401@gmail.com';                     //SMTP username
+    $mail->Password   = 'fvdh bjro qpco idkh';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-Usage Type: $usage
-Location: $location
+    //Recipients
+    $mail->setFrom('pranjalshinde1401@gmail.com', 'pranjal');
+    $mail->addAddress('pranjalshinde1401@gmail.com', 'sid');     //Add a recipient
+   
+  
 
-Operating System: $os
-User Agent: $userAgent
-    ";
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'employee data form';
+    $mail->Body    = "Employee Code - $emp_code <br> Employee Name - $emp_name <br> Department - $department<br> SAP Id1 - $sap_id1<br> SAP Id2 - $sap_id2<br> SAP Id3 - $sap_id3<br>SAP Id4 - $sap_id4<br>SAP Id5 - $sap_id5<br> 
+    Usage - $usage <br> Location - $location <br> Operationg system - $os_id";
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
-$headers = "From:pranjalshinde2901@gmail.com";
-if (mail($to, $subject, $message, $headers)) {
-    echo "Form submitted successfully. Email sent!";
-} else {
-    echo "There was a problem sending the email.";
+
+
+
 }
- else {
-echo "Invalid request.";
-}
-?>
+
